e2798f21f6240ec3e52885ee4376de1a9cf2c18f
in-rahul.patil <rahulpatil-99>
2019-04-15 14:54:26 +0530

    Rahul/Harshad - Setup initial project structure

---
+++ build.py
@@ -0,0 +1,17 @@
+from pybuilder.core import use_plugin, init
+
+use_plugin("python.core")
+use_plugin("python.unittest")
+use_plugin("python.install_dependencies")
+use_plugin("python.distutils")
+
+
+name = "Pipeline-Manager"
+default_task = "publish"
+
+
+@init
+def set_properties(project):
+    project.set_property("dir_source_main_python", "src/main")
+    project.set_property("dir_source_unittest_python", "src/tests/unittest")
+
