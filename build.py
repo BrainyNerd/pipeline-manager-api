0c132e282a006737ca6c0928813deb6732d5ccd9
in-rahul.patil <rahulpatil-99>
2019-04-16 09:42:15 +0530

    Rahul/Harshad - Add dummy server for initial setup

--- build.py
+++ build.py
@@ -4,6 +4,7 @@ use_plugin("python.core")
 use_plugin("python.unittest")
 use_plugin("python.install_dependencies")
 use_plugin("python.distutils")
+use_plugin("copy_resources")
 
 
 name = "Pipeline-Manager"
@@ -14,4 +15,6 @@ default_task = "publish"
 def set_properties(project):
     project.set_property("dir_source_main_python", "src/main")
     project.set_property("dir_source_unittest_python", "src/tests/unittest")
+    project.set_property("copy_resources_target", "$dir_dist")
+    project.install_file("/public","public/index.html")
 
