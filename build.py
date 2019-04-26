1766671b5c0f547c2c4de9b82b5132ae857f9e09
Chandrakant <in-chandrakant.kaski@in-chandrakatkaski.local>
2019-04-26 14:58:08 +0530

    [Chandrakant][#fix] update build file for python packaging

--- build.py
+++ build.py
@@ -23,12 +23,11 @@ def set_properties(project):
     project.set_property("dir_source_unittest_python", "src/tests/unittest")
     project.set_property("copy_resources_target", "$dir_dist")
     project.install_file("/public", "public/index.html")
-    project.install_file("/public/out", "./public/out/1776662db43bd611e4eb11d2add6d1c6.jpg")
+#     project.install_file("/public/out", "./public/out/1776662db43bd611e4eb11d2add6d1c6.jpg")
     project.install_file("/public/out", "./public/out/bundle.js")
     project.install_file("/public/out", "./public/out/styles.css")
     project.set_property('distutils_console_scripts', [
         "pipeline-manager = pipeline_manager.server:app.run"])
-
 
 @depends("install_runtime_dependencies")
 def publish():
--- ci-scripts/package.sh
+++ ci-scripts/package.sh
@@ -4,7 +4,8 @@ CONDA_DEVELOPMENT_ENV_NAME=pipeline_manager_dev
 export PATH="${PATH}:/root/miniconda3/bin"
 conda env create -f dev_environment.yml || echo "env pipeline_manager_dev already present"
 source activate ${CONDA_DEVELOPMENT_ENV_NAME}
-echo "*********** Packaging artip-cube s**************"
+pip install -r requirements.txt
+echo "*********** Packaging pipeline manager **************"
 pyb publish
 echo "*********** Packaging completed **************"
 conda deactivate
